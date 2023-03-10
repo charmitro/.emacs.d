fd71f9bf830afa325187093528e7ce7fda4a544f
Charalampos Mitrodimas <charmitro@gmail.com>
2023-03-10 23:31:53 +0200

    Initial commit
    Signed-off-by: Charalampos Mitrodimas <charmitro@gmail.com>

---
+++ lisp/init-mode-line.el
@@ -0,0 +1,11 @@
+;;; init-mode-line.el --- Settings and helpers mode-line
+;;; Commentary:
+;;; Code:
+
+(timeclock-mode-line-display 1)
+(display-time-mode 1)
+
+
+(provide 'init-mode-line)
+
+;;; init-mode-line.el ends here
