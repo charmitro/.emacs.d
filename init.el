a201fc331a3b4dcbf7ae49c8338c38b42cae10c7
Charalampos Mitrodimas <charmitro@gmail.com>
2023-03-11 18:48:18 +0200

    [KBD] add: keybinding configuration for MacOS
    Signed-off-by: Charalampos Mitrodimas <charmitro@gmail.com>

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
 (when (version< emacs-version "27.1")
   (message "Your emacs is old, and some functionality in this config will be disabled. PLease upgrade if possible."))
 
+(defconst *is-a-mac* (eq system-type 'darwin))
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'init-elpa)
 (require 'init-lisp)
@@ -15,6 +17,6 @@
 (require 'init-themes)
 (require 'init-mode-line)
 (require 'init-rust)
-
+(require 'init-osx)
 
 ;;; init.el ends here
