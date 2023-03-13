6fd2c5e98c20b1384e86c7c083dc64cb5e8a92d5
Panagiotis Foliadis <pfoliadis@hotmail.com>
2023-03-13 22:06:05 +0200

    [EDITOR] changed scroll behaviour
    Changed scrolling so that when the cursor
    reaches the end of the screen it doesn't
    automatically brings new information and
    goes to the middle of the screen but instead
    stays at the end of the screen and the new
    text appears line by line.
    
    Signed-off-by: Panagiotis Foliadis <pfoliadis@hotmail.com>

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -9,6 +9,11 @@
 (global-set-key (kbd "C-+") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
+;; nice scrolling
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+
 (provide 'init-editor)
 
 ;;; init-editor.el ends here.
