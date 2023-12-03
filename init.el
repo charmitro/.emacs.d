1d1f65374f88b058c0c3ef0d34242014514ad7d8
Charalampos Mitrodimas <charmitro@posteo.net>
2023-12-03 13:38:25 +0200

    Disable tool-bar, menu-bar & scroll-bar
    Signed-off-by: Charalampos Mitrodimas <charmitro@posteo.net>

--- init.el
+++ init.el
@@ -9,14 +9,15 @@
   (message "Your emacs is old, and some functionality in this config will be disabled. PLease upgrade if possible."))
 
 
-;; Bigger font on 4K
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 
+;; Bigger font on 4K
 (when (display-graphic-p)
   (if (= (x-display-pixel-width) 3840)
       (set-face-attribute 'default nil :height 200)
-    (set-face-attribute 'default nil :height 180))
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1))
+    (set-face-attribute 'default nil :height 180)))
 
 ;;; Disable sounds.
 (setq visible-bell t)
