98c0b7ae0e68af5869d9262aef10b5855a4ba0fe
Charalampos Mitrodimas <charmitro@posteo.net>
2024-03-02 00:55:23 +0200

    init-debug: feature update to search for debug words in project root
    Signed-off-by: Charalampos Mitrodimas <charmitro@posteo.net>

--- lisp/init-debug.el
+++ lisp/init-debug.el
@@ -4,15 +4,17 @@
 
 (global-set-key "\C-xpd" 'buffer-contains-debug-string)
 
-(defun buffer-contains-debug-string ()
-  "Search for the well-known Greek debug word $*@!*$ in the current buffer."
+(defun project-contains-debug-string ()
+  "Search for the well-known Greek debug word $*@!*$ in the current project."
   (interactive)
-  (let ((saved-line (line-number-at-pos)))
-    (goto-char (point-min))
-    (if (search-forward "peos" nil t)
-	(forward-line 0)
-      (forward-line (1- saved-line))
-      (message "No 'peos' found in the current buffer. You're free to commit!"))))
+  (if (require 'projectile nil 'noerror)
+      (let ((project-root (projectile-project-root)))
+	(if project-root
+	    (progn
+	      (grep (concat "grep --color=always -rnH --include=\"*.*\" -e 'peos\\|mouni\\|kavli' " project-root))
+	      (message "Searching for 'peos,mouni,kavli' in project: %s" project-root))
+	  (message "No 'peos,mouni,kavli' found in the current buffer. You're free to commit!")))
+	(message "Projectile is not available. Please install projectile package.")))
 
 (provide 'init-debug)
 
